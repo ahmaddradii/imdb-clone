42d7dfff7a0878098642067f4ac209db9338480c
Ahmad Radi <ahmaddradii9@gmail.com>
2023-03-20 15:28:39 +0200

    create the navbar component

---
+++ src/app/components/NavbarItem.jsx
@@ -0,0 +1,24 @@
+"use client";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+const NavbarItem = ({ title, param }) => {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+  return (
+    <div>
+      <Link
+        href={`/?genre=${param}`}
+        className={`m-4 p-2 hover:text-amber-600 font-semibold ${
+          genre &&
+          genre === param &&
+          "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+        }`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+};
+
+export default NavbarItem;
