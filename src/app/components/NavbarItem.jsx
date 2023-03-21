7ed38b94d729a5c44c57d5c88fd780f1e5b76233
ahmaddradii <ahmadradi.dev@gmail.com>
2023-03-21 21:59:53 +0200

    fetch data from tmdb api in the home page

--- src/app/components/NavbarItem.jsx
+++ src/app/components/NavbarItem.jsx
@@ -1,24 +1,24 @@
 "use client";
+
 import Link from "next/link";
+
 import { useSearchParams } from "next/navigation";
 
-const NavbarItem = ({ title, param }) => {
+export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
   return (
     <div>
       <Link
-        href={`/?genre=${param}`}
-        className={`m-4 p-2 hover:text-amber-600 font-semibold ${
+        className={`m-4 hover:text-amber-600 font-semibold p-2 ${
           genre &&
           genre === param &&
-          "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+          "underline underline-offset-8 decoration-4  decoration-amber-500 rounded-lg"
         }`}
+        href={`/?genre=${param}`}
       >
         {title}
       </Link>
     </div>
   );
-};
-
-export default NavbarItem;
+}
