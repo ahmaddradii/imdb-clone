70f403946dc725669edcc4e10af94b4efd097e55
ahmaddradii <ahmadradi.dev@gmail.com>
2023-03-24 03:26:51 +0200

    update movie page

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,29 +1,32 @@
 import Image from "next/image";
 
-const getMovie = async (movieId) => {
+async function getMovie(movieId) {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
   );
-
   return await res.json();
-};
-const MoviePage = async ({ params }) => {
+}
+
+export default async function MoviePage({ params }) {
   const movieId = params.id;
   const movie = await getMovie(movieId);
   return (
     <div className="w-full">
-      <div className="max-w-6xl mx-auto md:space-x-6 p-4 md:pt-8 flex flex-col md:flex-row items-center content-center">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
         <Image
           src={`https://image.tmdb.org/t/p/original/${
-            result.backdrop_path || result.poster_path
+            movie.backdrop_path || movie.poster_path
           }`}
           width={500}
           height={300}
+          className="rounded-lg"
+          style={{
+            maxWidth: "100%",
+            height: "100%",
+          }}
           placeholder="blur"
           blurDataURL="/spinner.svg"
-          style={{ width: "100%", height: "100%" }}
-          className="rounded-lg"
-          alt="movie poster"
+          alt="Movie poster"
         ></Image>
         <div className="p-2">
           <h2 className="text-lg mb-3 font-bold">
@@ -45,6 +48,4 @@ const MoviePage = async ({ params }) => {
       </div>
     </div>
   );
-};
-
-export default MoviePage;
+}
